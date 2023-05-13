580a166d33ec4b52fdc31f13e94936c9986d1ad7
jordan <jordan@bitola.com>
2023-05-13 19:52:27 +0100

    Standard tooling - deploy shell scripts

---
+++ deployLineaTestnet.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Run anvil.sh in another shell before running this
+
+# To load the variables in the .env file
+source .env
+
+# To deploy and verify our contract
+forge script script/DeployLineaTestnet.s.sol:Deploy --slow --legacy --rpc-url "https://rpc.goerli.linea.build" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+
+source push_artifacts.sh "DeployLineaTestnet.s.sol/59140"
+
+# cd web
+# npm run build
