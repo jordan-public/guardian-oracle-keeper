80f33acc60ce4d6bb0253792544e622229c57a3a
jordan <jordan@bitola.com>
2023-05-13 19:57:18 +0100

    Tooling fix

--- push_artifacts.sh
+++ push_artifacts.sh
@@ -8,10 +8,10 @@ for dirname in out/*.sol; do
     cat $dirname/$(basename "$dirname" .sol).json | jq '{abi: .abi}' > web/src/artifacts/$(basename "$dirname" .sol).json
 done
 
-cat broadcast/$1/run-latest.json out/CFDOrderBookFactory.sol/CFDOrderBookFactory.json | \
+cat broadcast/$1/run-latest.json out/GuardianFactory.sol/GuardianFactory.json | \
 jq -s \
     'add | 
     { chain: .chain} * (.transactions[] |
     { transactionType, contractName, contractAddress } |
-    select(.transactionType == "CREATE" and .contractName == "CFDOrderBookFactory") |
-    {contractName, contractAddress}) * {abi: .abi}' > web/src/artifacts/CFDOrderBookFactory.json
+    select(.transactionType == "CREATE" and .contractName == "GuardianFactory") |
+    {contractName, contractAddress}) * {abi: .abi}' > web/src/artifacts/GuardianFactory.json
