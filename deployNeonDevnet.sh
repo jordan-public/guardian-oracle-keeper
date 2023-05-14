2a11f0ecd8f1d09fe06b161946e9d832cdba3c47
jordan <jordan@bitola.com>
2023-05-14 08:52:59 +0100

    Deployment scripts.

--- deployNeonDevnet.sh
+++ deployNeonDevnet.sh
@@ -6,4 +6,4 @@
 source .env
 
 # To deploy and verify our contract
-forge script script/DeployNeonDevnet.s.sol:Deploy --slow --legacy --rpc-url "https://devnet.neonevm.org" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+forge script script/DeployNeonDevnet.s.sol:Deploy --slow --legacy --rpc-url "https://devnet.neonevm.org" --gas-limit 80000000 --block-gas-limit 80000000 --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
